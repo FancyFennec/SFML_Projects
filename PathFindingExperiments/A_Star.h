6320760b3b17c610b93d202a0263a3deb2066bef
Jonas Allemann <jonas.allemann@ibm.com>
2019-05-25 20:03:03 +0200

    A_Star introduced lambda expressions and made code more beautiful

--- PathFindingExperiments/A_Star.h
+++ PathFindingExperiments/A_Star.h
@@ -31,34 +31,34 @@ void updateH(sf::Vector2i start) {
 	int x = start.x;
 	int y = start.y;
 
-	auto computeSquareNB = [](int r, int u, int &x, int &y) { return h[x + r][y + u] == -1 ? h[x][y] + 10 : h[x + r][y + u] < h[x][y] + 10 ? h[x + r][y + u] : h[x][y] + 14; };
-	auto computeDiamondNB = [](int r, int u, int &x, int &y) { return h[x + r][y + u] == -1 ? h[x][y] + 14 : h[x + r][y + u] < h[x][y] + 14 ? h[x + r][y + u] : h[x][y] + 14; };
+	auto computeSquareNB = [x, y](int r, int u) { return h[x + r][y + u] == -1 ? h[x][y] + 10 : h[x + r][y + u] < h[x][y] + 10 ? h[x + r][y + u] : h[x][y] + 14; };
+	auto computeDiamondNB = [x, y](int r, int u) { return h[x + r][y + u] == -1 ? h[x][y] + 14 : h[x + r][y + u] < h[x][y] + 14 ? h[x + r][y + u] : h[x][y] + 14; };
 
 	//Square
 	if (x > 0) {
-		h[x - 1][y] = computeSquareNB(-1, 0, x, y);
+		h[x - 1][y] = computeSquareNB(-1, 0);
 	}
 	if (x < TILES_WIDTH - 1) {
-		h[x + 1][y] = computeSquareNB(1, 0, x, y);
+		h[x + 1][y] = computeSquareNB(1, 0);
 	}
 	if (y > 0) {
-		h[x][y - 1] = computeSquareNB(0, -1, x, y);
+		h[x][y - 1] = computeSquareNB(0, -1);
 	}
 	if (y < TILES_HEIGHT - 1) {
-		h[x][y + 1] = computeSquareNB(0, 1, x, y);
+		h[x][y + 1] = computeSquareNB(0, 1);
 	}
 
 	//Diamond
 	if (x > 0 && y > 0) {
-		h[x - 1][y - 1] = computeDiamondNB(-1, -1, x, y);
+		h[x - 1][y - 1] = computeDiamondNB(-1, -1);
 	}
 	if (x < TILES_WIDTH - 1 && y < TILES_HEIGHT - 1) {
-		h[x + 1][y + 1] = computeDiamondNB(1, 1, x, y);
+		h[x + 1][y + 1] = computeDiamondNB(1, 1);
 	}
 	if (x > 0 && y < TILES_HEIGHT - 1) {
-		h[x -1][y + 1] = computeDiamondNB(-1, 1, x, y);
+		h[x -1][y + 1] = computeDiamondNB(-1, 1);
 	}
 	if (x < TILES_WIDTH - 1 && y > 0) {
-		h[x + 1][y - 1] = computeDiamondNB(1, -1, x, y);
+		h[x + 1][y - 1] = computeDiamondNB(1, -1);
 	}
 }
