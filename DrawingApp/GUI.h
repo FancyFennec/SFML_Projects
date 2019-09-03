c701664cba7ecf21fc71b2f5366d560af5897515
Jonas Allemann <jonas.allemann@ibm.com>
2019-09-03 21:53:58 +0100

    da_scenes finaly got the popup to work properly

--- DrawingApp/GUI.h
+++ DrawingApp/GUI.h
@@ -89,77 +89,79 @@ void mainMenuGUI(sf::RenderWindow& mainWindow, Scene& scene)
 		ImGui::EndMainMenuBar();
 
 		if (popupIsOpen) {
-			ImGui::OpenPopup("SavedFiles Folder");
-			
-			std::string folderPath("./SavedFiles");
-			for (auto p : fs::directory_iterator(folderPath)) {
-				std::string fileName = p.path().string().substr(folderPath.size() + 1); //Remove folder name
-				if (ImGui::Button(fileName.data(), ImVec2(200, 20))) {
-					input = fileName.substr(0, fileName.length() - 4).c_str(); //remove ending
-				}
-			}
-
-			ImGui::Dummy(ImVec2(0, 10));
-			ImGui::DrawLine(sf::Vector2f(0, 0), sf::Vector2f(WINDOW_WIDTH, 0), sf::Color::White, 1.0f);
-			ImGui::Dummy(ImVec2(0, 20));
-
-			char* inputChar = (char*) input.data();
-			if (ImGui::InputText("Press Enter", inputChar, 30, ImGuiInputTextFlags_EnterReturnsTrue)) {
-				std::string file = "./SavedFiles/";
-				file.append(inputChar);
-				switch (file_type) {
-				case(JPEG, SINGLE_JPEG) :
-					file.append(".jpg");
-					break;
-				case(PNG, SINGLE_PNG):
-					file.append(".png");
-					break;
-				case(SCN):
-					file.append(".scn");
-					break;
-				}
-				
-				sf::RenderTexture rTex;
-				rTex.create(scene.width, scene.height);
-				switch (file_type) {
-				case(JPEG): {
-					rTex.clear(sf::Color::White);
-					for (auto layer = scene.layers.begin(); layer != scene.layers.end(); layer++) {
-						rTex.draw(layer->sprite);
+			ImGui::OpenPopup("Save Window");
+			if (ImGui::BeginPopupModal("Save Window", &popupIsOpen)) {
+				std::string folderPath("./SavedFiles");
+				for (auto p : fs::directory_iterator(folderPath)) {
+					std::string fileName = p.path().string().substr(folderPath.size() + 1); //Remove folder name
+					if (ImGui::Button(fileName.data(), ImVec2(200, 20))) {
+						input = fileName.substr(0, fileName.length() - 4).c_str(); //remove ending
 					}
-					break;
-				}
-				case(PNG): {
-					rTex.clear(sf::Color(255, 255, 255, 0));
-					for (auto layer = std::next(scene.layers.begin()); layer != scene.layers.end(); layer++) {
-						rTex.draw(layer->sprite);
-					}
-					break;
-				}
-				case(SINGLE_JPEG): {
-					rTex.clear(sf::Color::White);
-					rTex.draw(scene.layers.begin()->sprite);
-					rTex.draw(scene.currentLayer->sprite);
-					break;
-				}
-				case(SINGLE_PNG): {
-					rTex.clear(sf::Color(255, 255, 255, 0));
-					rTex.draw(scene.currentLayer->sprite);
-					break;
-				}
-				case(SCN): {
-					//TODO: Create Folder and save all the textures;
-				}
 				}
 
-				rTex.display();
-				rTex.getTexture().copyToImage().saveToFile(file);
-				
-				popupIsOpen = false;
+				ImGui::Dummy(ImVec2(0, 10));
+				ImGui::DrawLine(sf::Vector2f(0, 0), sf::Vector2f(WINDOW_WIDTH, 0), sf::Color::White, 1.0f);
+				ImGui::Dummy(ImVec2(0, 20));
+
+				char* inputChar = (char*)input.data();
+				if (ImGui::InputText("Press Enter", inputChar, 30, ImGuiInputTextFlags_EnterReturnsTrue)) {
+					std::string file = "./SavedFiles/";
+					file.append(inputChar);
+					switch (file_type) {
+					case(JPEG, SINGLE_JPEG):
+						file.append(".jpg");
+						break;
+					case(PNG, SINGLE_PNG):
+						file.append(".png");
+						break;
+					case(SCN):
+						file.append(".scn");
+						break;
+					}
+
+					sf::RenderTexture rTex;
+					rTex.create(scene.width, scene.height);
+					switch (file_type) {
+					case(JPEG): {
+						rTex.clear(sf::Color::White);
+						for (auto layer = scene.layers.begin(); layer != scene.layers.end(); layer++) {
+							rTex.draw(layer->sprite);
+						}
+						break;
+					}
+					case(PNG): {
+						rTex.clear(sf::Color(255, 255, 255, 0));
+						for (auto layer = std::next(scene.layers.begin()); layer != scene.layers.end(); layer++) {
+							rTex.draw(layer->sprite);
+						}
+						break;
+					}
+					case(SINGLE_JPEG): {
+						rTex.clear(sf::Color::White);
+						rTex.draw(scene.layers.begin()->sprite);
+						rTex.draw(scene.currentLayer->sprite);
+						break;
+					}
+					case(SINGLE_PNG): {
+						rTex.clear(sf::Color(255, 255, 255, 0));
+						rTex.draw(scene.currentLayer->sprite);
+						break;
+					}
+					case(SCN): {
+						//TODO: Create Folder and save all the textures;
+					}
+					}
+
+					rTex.display();
+					rTex.getTexture().copyToImage().saveToFile(file);
+
+					popupIsOpen = false;
+				}
+				ImGui::Dummy(ImVec2(0, 30));
+				ImGui::SameLine(ImGui::GetWindowSize().x - 60);
+				if (ImGui::Button("Close", ImVec2(50, 20))) popupIsOpen = false;
+				ImGui::EndPopup();
 			}
-			ImGui::Dummy(ImVec2(0, 30));
-			ImGui::SameLine(ImGui::GetWindowSize().x - 60);
-			if (ImGui::Button("Close", ImVec2(50, 20))) popupIsOpen = false;
 		}
 	}
 }
