4dc45e26ffb1b72164304e103161930d9b760f6d
Jonas Allemann <jonas.allemann@ibm.com>
2019-09-08 21:38:37 +0100

    da_scenes created more brushes

--- Tests/Source.cpp
+++ Tests/Source.cpp
@@ -1,38 +1,101 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 #include <algorithm> 
 #include <vector> 
 #include <filesystem>
+#include "Source.h"
 namespace fs = std::experimental::filesystem;
 
+sf::RenderWindow window;
+sf::Event event;
+sf::Color clear(0, 0, 0, 255);
+unsigned int width = 256;
+
+sf::Image img;
+sf::Texture tex;
+sf::Sprite sprite;
+
+int step = 0;
+bool calculateNewStep = true;
+unsigned int fileCounter = 0;
+
+float getAlpha(int x, int y, float p) { 
+	return std::powf(1.0f - (float)(x * x + y * y) / (width * width / 4), p);
+};
+
+void calculatePixels(double p);
+
 int main() {
-	/*bool folder_found = false;
-	for (auto p : fs::directory_iterator("./TestFolders")) {
-		if (fs::is_directory(p)) {
-			folder_found = true;
-			bool files_found = false;
-			std::cout << std::string("Hooray! Folder found: ").append(p.path().string().data()) << std::endl;
-			for (auto s : fs::directory_iterator(p.path())) {
-				if (!fs::is_directory(s)) {
-					files_found = true;
-					std::cout << std::string("Hooray! There were files in the folder: ").append(s.path().string()) << std::endl;
-				}
-				else {
-					std::cout << std::string("Hooray! Found another folder. ") << std::endl;
+
+	window.create(sf::VideoMode(width, width), "Drawing App", sf::Style::Titlebar);
+	window.setMouseCursorVisible(false);
+	window.setFramerateLimit(60);
+
+	while (window.isOpen()) {
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::KeyPressed) {
+				if (event.key.code == sf::Keyboard::Escape) {
+					window.close();
 				}
 			}
-			if (!files_found) {
-				std::cout << std::string("No files found in: ").append(p.path().string().data()) << std::endl;
+			if (event.type == sf::Event::KeyPressed) {
+				if (event.key.code == sf::Keyboard::Up) {
+					calculateNewStep = true;
+					step += 1;
+				}
+			}
+			if (event.type == sf::Event::KeyPressed) {
+				if (event.key.code == sf::Keyboard::Down) {
+					calculateNewStep = true;
+					step -= 1;
+				}
+			}
+			if (event.type == sf::Event::KeyPressed) {
+				if (event.key.code == sf::Keyboard::S) {
+					std::string filename = "Brush";
+					filename.append(std::to_string(fileCounter)).append(".png");
+					img.saveToFile(filename);
+					fileCounter++;
+				}
 			}
 		}
+
+		if (calculateNewStep) {
+			img.create(width, width, sf::Color(255, 255, 255, 0));
+			calculatePixels(pow(2, step));
+			tex.create(width, width);
+			tex.loadFromImage(img);
+			sprite.setTexture(tex);
+
+			calculateNewStep = false;
+		}
+
+		window.clear(clear);
+
+		window.draw(sprite);
+
+		window.display();
 	}
-	if (!folder_found) {
-		std::cout << std::string("No files found in ./TestFolders") << std::endl;
-	}*/
-
-	char test[128] = "Hallo";
-	strcpy_s(test, "Echo");
-
-	std::cout << test << std::endl;
+	
 
 	return 0;
+}
+
+void calculatePixels(double p)
+{
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < width; j++) {
+			int x = width / 2 - i;
+			int y = width / 2 - j;
+
+			if ((x * x + y * y) <= (width * width / 4)) {
+				float alpha = getAlpha(x, y, p) * 255;
+				img.setPixel(i, j, sf::Color(255, 255, 255, alpha));
+			}
+			else {
+				img.setPixel(i, j, sf::Color(255, 255, 255, 0));
+			}
+
+		}
+	}
 }
