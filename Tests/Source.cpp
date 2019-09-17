1fa159c95899164086a46151f7aecfb662dc6ddc
Jonas Allemann <jonas.allemann@ibm.com>
2019-09-17 21:03:29 +0100

    started to work on saving the scenes

--- Tests/Source.cpp
+++ Tests/Source.cpp
@@ -1,23 +1,43 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <fstream>
 #include <algorithm> 
 #include <vector> 
 #include <filesystem>
 #include "Source.h"
 #include "TestClass.h"
+#include "msgpack.hpp"
 namespace fs = std::experimental::filesystem;
 
-void func(int a, void(*f)(int, float)) {
-	f(a, 2.0);
-}
-
-void print(int a, float b) {
-	std::cout << a * b << std::endl;
-}
-
 int main() {
-	int x = 2;
-	TestClass::increaseNumber(x);
-	TestClass::increaseNumber(x);
-	func(TestClass::getNumber(), print);
+
+	std::ofstream ostrm("MyFile.jns", std::ios::out | std::ios::binary);
+	std::vector<int> a = { 1, 2, 3 };
+	
+	msgpack::sbuffer sbuf;
+	msgpack::pack(sbuf, a);
+	std::string data = sbuf.data();
+	ostrm << data;
+	ostrm.close();
+
+	std::ifstream istrm("MyFile.jns", std::ios::in | std::ios::binary);
+
+	istrm.seekg(0, std::ifstream::end);
+	int size = istrm.tellg() / sizeof(char);
+	char* buffer = new char(size);
+	istrm.seekg(0, std::ifstream::beg);
+
+	istrm.read((char*)buffer, sizeof(size));
+	std::string data_back(buffer);
+
+	// get your data safely back
+	msgpack::unpacked msg = msgpack::unpack(data_back.data(), data_back.size());
+	msgpack::object obj = msg.get();
+
+	// now convert the bytes back to your objects
+	std::vector<int> b;
+	obj.convert(b);
+	for (int x : b) {
+		std::cout << x << std::endl;
+	}
 }
