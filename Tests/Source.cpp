da414117042ed1a920d613f5fe04155200d9b8fd
Jonas Allemann <jonas.allemann@ibm.com>
2019-09-18 21:49:27 +0100

    lerp_thread able read and write my own file format

--- Tests/Source.cpp
+++ Tests/Source.cpp
@@ -9,55 +9,59 @@
 #include "msgpack.hpp"
 namespace fs = std::experimental::filesystem;
 
-std::vector<sf::Uint8> createVector(sf::Image image) {
-	sf::Vector2u size = image.getSize();
-	int width = size.x;
-	int height = size.y;
-	std::vector<sf::Uint8> data = {};
-	data.push_back(width);
-	data.push_back(height);
-
-	for (int j = 0; j < width; j++) {
-		for (int i = 0; i < height; i++) {
-			sf::Color col = image.getPixel(i, j);
-			data.push_back(col.r);
-			data.push_back(col.g);
-			data.push_back(col.b);
-			data.push_back(col.a);
-		}
-	}
-	return data;
-}
-
 int main() {
 
-	std::ofstream ostrm("MyFile.jns", std::ios::out | std::ios::binary);
-	
-	std::vector<sf::Uint8> a = {};
 	sf::Image img;
-	int width = 200;
-	int height = 200;
-	img.create(width, height, sf::Color::Green);
+	int width = 1200;
+	int height = 800;
+	img.create(width, height, sf::Color::Red);
+	img.saveToFile("test.png");
+
+	std::ifstream istrm("test.png", std::ios::in | std::ios::binary);
+	std::ofstream ostrm("MyFile.jns", std::ios::out | std::ios::binary);
+
 	ostrm.write((char*)&width, sizeof(int));
 	ostrm.write((char*)&height, sizeof(int));
 
-	ostrm.write((char*)&img.getPixelsPtr()[0], sizeof(sf::Uint8) * width * height * 4);
+	istrm.seekg(0, std::ios::end);
+	std::streampos end = istrm.tellg();
+	istrm.seekg(0, std::ios::beg);
+
+	std::vector<char> buffer;
+	buffer.reserve(end / sizeof(char));
+	buffer.resize(end / sizeof(char));
+
+	istrm.read((char*)buffer.data(), end / sizeof(char));
+	ostrm.write((char*)buffer.data(), end / sizeof(char));
+	
+	istrm.close();
 	ostrm.close();
 
-	std::ifstream istrm("MyFile.jns", std::ios::in | std::ios::binary);
+	std::ifstream newistrm("MyFile.jns", std::ios::out | std::ios::binary);
+	int new_width;
+	newistrm.read((char*)&new_width, sizeof(int));
+	int new_height;
+	newistrm.read((char*)&new_height, sizeof(int));
 
-	int new_width = 0;
-	istrm.read((char*)&new_width, sizeof(int));
-	int new_height = 0;
-	istrm.read((char*)&new_height, sizeof(int));
-	
+	std::cout << new_width << std::endl;
+	std::cout << new_height << std::endl;
+
 
 	sf::Image image;
 	image.create(new_width, new_height);
-	istrm.read((char*)&image.getPixelsPtr()[0], sizeof(sf::Uint8) * new_width * new_height * 4);
-	istrm.close();
 
-	image.saveToFile("test.jpg");
-	
+	std::streampos begin = newistrm.tellg();
+	newistrm.seekg(0, std::ios::end);
+	end = newistrm.tellg();
+	newistrm.seekg(begin);
+
+	buffer.reserve((end - begin)/ sizeof(char));
+	buffer.resize((end - begin) / sizeof(char));
+	newistrm.read((char*)buffer.data(), (end - begin) / sizeof(char));
+
+	newistrm.close();
+
+	image.loadFromMemory(buffer.data(), new_width * new_height);
+	image.saveToFile("test2.png");
 
 }
